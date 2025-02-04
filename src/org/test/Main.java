11f6c1fd641c61aa0b88ab5e4bbc5702b9b6699d
Oleh Savenko <47480765+osavenko@users.noreply.github.com>
2025-02-04 08:51:09 +0000

    Format committed

--- src/org/test/Main.java
+++ src/org/test/Main.java
@@ -1,8 +1,9 @@
 package org.test;
 
-public class Main{
-    public static void main(String[] args){
+public class Main {
+
+    public static void main(String[] args) {
         System.out.print("Hello world");
-        
+
     }
 }
